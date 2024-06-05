7e08e2204df8c200baf9971d8d30b1105a99d2b5
Peter Wolf <pwolf2310@gmail.com>
2024-06-06 00:02:50 +0200

    use q-args to make the python example more flexible

--- examples/python/dev_run_loop.vim
+++ examples/python/dev_run_loop.vim
@@ -1,7 +1,7 @@
 " Check if the vim-live-preview plugin is installed
 if &rtp =~ 'vim-live-preview'
-  autocmd FileType python command! VLPPython call
-        \ vlp#EnterPreviewMode(':!python3 <fname> --verbose $USER',
+  autocmd FileType python command! -nargs=? VLPPython call
+        \ vlp#EnterPreviewMode(":!python3 <fname> " . <q-args>,
         \ {
         \   'input': 'fname',
         \   'preview_buffer_name': 'Python Output',
--- examples/python/main.py
+++ examples/python/main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+
+# Source dev_run_loop.vim
+# Invoke the command VLPPython with optional arguments, for example:
+# :VLPPython -v John
+# :VLPPython --verbose
+# :VLPPython
 
 def main():
+    # get $USER
     parser = argparse.ArgumentParser()
-    parser.add_argument("name", help="name of the person")
+    parser.add_argument("-n", "--name", help="name of the person", default=os.getenv("USER"))
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args()
     if args.verbose:
